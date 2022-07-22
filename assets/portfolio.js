cc8f4da90e9a7a03157e82d276f8c9f6867391db
ItsWitherMC <54366349+ItsWitherMC@users.noreply.github.com>
2022-07-22 17:43:51 +0700

    owo uwu qwq twt (yes i have brain problems but first commit tho yay!)

---
+++ assets/portfolio.js
@@ -0,0 +1,10 @@
+let portfolio = 
+[
+    {
+        "name": "AntiChatReports",
+        "description": "hi person reading this from github",
+        "link": "NO LONGER MAINTAINED",
+        "type": "plugin",
+        "featured": 1
+    },
+].sort((a, b) => a.name.localeCompare(b.name))
